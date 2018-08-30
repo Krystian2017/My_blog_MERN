ecfba945e350dad3ab1c38db55c77ea03788c2ba
Krystian Bieniek <krystekk2013@gmail.com>
2018-08-30 20:26:09 +0200

    Change in edit post

--- client/modules/Post/PostActions.js
+++ client/modules/Post/PostActions.js
@@ -4,6 +4,7 @@ import callApi from '../../util/apiCaller';
 export const ADD_POST = 'ADD_POST';
 export const ADD_POSTS = 'ADD_POSTS';
 export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
 
 // Export Actions
 export function addPost(post) {
@@ -61,9 +62,9 @@ export function deletePostRequest(cuid) {
 
 export function editPost(cuid, post) {
   return {
-    type:   EDIT_POST,
+    type: EDIT_POST,
     cuid,
-    post,
+    post
   };
 }
 
